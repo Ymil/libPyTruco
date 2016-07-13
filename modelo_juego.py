ca17c70a13f3f8a7c00c041aa01330ae70442958
Lautaro Linquiman <acc.limayyo@gmail.com>
2016-07-13 08:16:54 -0300

    Trabajando en las dbs

--- modelo_juego.py
+++ modelo_juego.py
@@ -1,13 +1,32 @@
 import sqlite3 as lite
-
+import json
 class modelo_juego():
     def __init__(self):
-        self.con = lite.connect('.libPyTruco.db')
+        self.con = lite.connect('.juego.db')
         self.conStable = False
         with self.con:
             self.conStable = True
             self.conCursor = self.con.cursor()
-    def __createTables(self):
-        if self.conStable:
-            self.conCursor.execute('''CREATE TABLE mano
-                (date text, trans text, symbol text, qty real, price real)''')
+            self.getInfoTable(1)
+
+    def getInfoTable(self, id):
+        self.conCursor.execute("select * from mesa where id = %d" % id)
+        print type(self.conCursor),dir(self.conCursor)
+        x = [row for row in self.conCursor][0]
+        print x,type(x),x[0]
+        print str(x[2])
+        #rep = json.load(self.conCursor[2])
+        #print rep
+'''
+python
+
+from modelo_juego import modelo_juego as mj
+
+jm = mj()
+
+exit()
+
+python
+
+
+'''
