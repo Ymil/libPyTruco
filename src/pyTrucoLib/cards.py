2dd0b89f3814db36fb08d1f5c5e9271559e40d6d
Lautaro Linquiman <lylinquiman@gmail.com>
2022-06-11 21:45:44 -0300

    Se actualiza la clase card

--- src/pyTrucoLib/cards.py
+++ src/pyTrucoLib/cards.py
@@ -7,59 +7,18 @@ __date__ = '06-01-2015'
 
 from copy import copy
 from random import randrange
-from .card import Card
+from .card import Card, cards_dict
 
 
 class Cards():
     '''Clase encargada de el manejo de las cartas del juego'''
 
     def __init__(self):
-        '''
-        Puntaje de cada carta
-        4 = 1
-        5 = 2
-        6 = 3
-        7 Basto y Copa = 4
-        10 = 5
-        11 = 6
-        12 = 7
-        As Oro Y Copo = 8
-        2 = 9
-        3 = 10
-        7 Oro = 11
-        7 Espada = 12
-        1 Basto = 13
-        1 Espada = 14
-        '''
-        self.cartas = [
-            # [Numero de carta, palo, valor]
-            [1, 'oro', 8],  [1, 'espada', 14],
-            [1, 'basto', 13], [1, 'copa', 8],
-            [2, 'oro', 9],  [2, 'espada', 9],
-            [2, 'basto', 9],  [2, 'copa', 9],
-            [3, 'oro', 10], [3, 'espada', 10],
-            [3, 'basto', 10], [3, 'copa', 10],
-            [4, 'oro', 1],  [4, 'espada', 1],
-            [4, 'basto', 1],  [4, 'copa', 1],
-            [5, 'oro', 2],  [5, 'espada', 2],
-            [5, 'basto', 2],  [5, 'copa', 2],
-            [6, 'oro', 3],  [6, 'espada', 3],
-            [6, 'basto', 3],  [6, 'copa', 3],
-            [7, 'oro', 11], [7, 'espada', 12],
-            [7, 'basto', 4],  [7, 'copa', 4],
-            [10, 'oro', 5], [10, 'espada', 5],
-            [10, 'basto', 5], [10, 'copa', 5],
-            [11, 'oro', 6], [11, 'espada', 6],
-            [11, 'basto', 6], [11, 'copa', 6],
-            [12, 'oro', 7], [12, 'espada', 7],
-            [12, 'basto', 7], [12, 'copa', 7],
-        ]
-
         self.clonCartas = []
         self.cartasRepartidas = []
 
     def __clonarCartas(self):
-        self.clonCartas = copy(self.cartas)
+        self.clonCartas = copy(cards_dict)
 
     def prepararMaso(self):
         '''Esta funcion copia los valores y nombres de las
@@ -72,13 +31,13 @@ class Cards():
         Return list (1,2,3)'''
 
         cartasJugador = []  # Acomoda las cartas de cada jugador
-
+        cards_keys = self.clonCartas.keys()
         for _ in range(3):  # reparte tres cartas aleatorias
-            cartaN = randrange(0, len(self.cartas) - 1)
-            cartaList = self.cartas[cartaN]
-            cartaObject = Card(cartaList)
+            cartaN = randrange(0, len(cards_keys) - 1)
+            card_name = self.cartas[cartaN]
+            cartaObject = Card(card_name)
             cartasJugador.append(cartaObject)
-            self.cartas.remove(cartaList)
+            del self.clonCartas[card_name]
             del cartaObject
 
         return cartasJugador
